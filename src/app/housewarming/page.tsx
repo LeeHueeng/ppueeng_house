c83a12dd33893f17be3f41d6edbc4d7328054516
LeeHueeng <zzxx373014@gmail.com>
2024-02-06 12:08:06 +0900

    이미지 페이지 제작

--- src/app/housewarming/page.tsx
+++ src/app/housewarming/page.tsx
@@ -1,6 +1,31 @@
+"use client";
 import Image from "next/image";
 import styles from "../page.module.css";
+import { useState } from "react";
+/*@__CLIENT__*/
 export default function houswarming() {
+  const imagePaths = [
+    "/house/sample_images_01.png",
+    "/house/sample_images_02.png",
+    "/house/sample_images_03.png",
+    "/house/sample_images_04.png",
+    "/house/sample_images_05.png",
+    "/house/sample_images_06.png",
+  ];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleSwipe = (direction: string) => {
+    if (direction === "left") {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? imagePaths.length - 1 : prevIndex - 1
+      );
+    } else if (direction === "right") {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === imagePaths.length - 1 ? 0 : prevIndex + 1
+      );
+    }
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -26,34 +51,24 @@ export default function houswarming() {
           </a>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/house/sample_images_01.png"
-          alt="Next.js Logo"
-          width={600}
-          height={300}
-          priority
-        />
-        <Image
-          className={styles.logo}
-          src="/house/sample_images_02.png"
-          alt="Next.js Logo"
-          width={600}
-          height={300}
-          priority
-        />
-        <Image
-          className={styles.logo}
-          src="/house/sample_images_03.png"
-          alt="Next.js Logo"
-          width={600}
-          height={300}
-          priority
-        />
+      <div className={styles.centerflex}>
+        <div className={styles.arrow} onClick={(e) => handleSwipe("left")}>
+          &lt;
+        </div>
+        <div className={styles.centerimg}>
+          <Image
+            className={styles.logo}
+            src={imagePaths[currentIndex]}
+            alt={`Slide ${currentIndex + 1}`}
+            width={600}
+            height={300}
+            priority
+          />
+        </div>
+        <div className={styles.arrow} onClick={() => handleSwipe("right")}>
+          &gt;
+        </div>
       </div>
-
       <div className={styles.grid}>
         <a
           href="/housewarming"
